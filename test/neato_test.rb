66819f966e4d931f271ffa55258d904032acfb59
Ken Collins <ken@metaskills.net>
2017-03-15 09:14:08 -0400

    Baisc test setup.

--- test/neato_test.rb
+++ test/neato_test.rb
@@ -1,11 +1,9 @@
 require 'test_helper'
 
-class NeatoTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Neato::VERSION
+class NeatoTest < Minitest::Spec
+
+  it 'foo' do
+    assert true
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
