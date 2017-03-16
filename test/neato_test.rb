afae547464f7b613e8c7a93a3e7eadd715144a64
Ken Collins <ken@metaskills.net>
2017-03-16 11:19:16 -0400

    Teset Neat v1.8 rendering.

--- test/neato_test.rb
+++ test/neato_test.rb
@@ -2,12 +2,16 @@ require 'test_helper'
 
 class NeatoTest < Neato::Spec
 
-  it 'can import neat' do
+  it 'can import neat v1 by default' do
     assert_sass <<-SASS, <<-CSS
       @import 'neat';
+      $mobile: new-breakpoint(max-width 480px 4);
+      .test { @include span-columns(6 of 6); }
     SASS
       html { box-sizing: border-box; }
       *, *::after, *::before { box-sizing: inherit; }
+      .test { float: left; display: block; margin-right: 4.82916%; width: 100%; }
+      .test:last-child { margin-right: 0; }
     CSS
   end
 
